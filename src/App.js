283f4e0ad18818d88ce438ad375d64ff9fb44f0d
minholeelog <91aiden.lee@gmail.com>
2022-02-04 01:06:29 +0900

    Context 활용

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
-import './App.css';
+import './App.css'
+import Auth from './components/Auth'
+import Header from './components/Header'
+import { UserContextProvider } from './context/userContext'
 
 function App() {
-  return <div className='App'></div>;
+  return (
+    <div className="ui container">
+      <UserContextProvider>
+        <Header />
+        <Auth />
+      </UserContextProvider>
+    </div>
+  )
 }
 
-export default App;
+export default App
